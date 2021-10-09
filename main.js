ab7b7101b42792a6be1d78c6988c0736a6f719c4
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 10:40:14 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+https://wix.github.io/react-native-navigation/docs/passing-data-to-components/
+https://reactnative.dev/docs/getting-started
+https://reactnative.dev/docs/components-and-apis
+https://devdocs.io/react_native/
+https://docs.sentry.io/platforms/react-native/
+https://react-native-elements.github.io/react-native-elements/docs/getting_started.html
